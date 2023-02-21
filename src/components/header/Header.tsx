e429798c52e59c0b3e5e43b83442b15ab17f5476
Omi <digicoder@protonmail.com>
2023-02-21 12:06:29 -0500

    added header component and its styling file.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import '../../styles/header.css';
+
+const Header = function () {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+      <nav className="header__nav">
+        <ul className="header__nav__list">
+          <li className="header__nav__list__items"></li>
+          <li className="header__nav__list__items"></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
